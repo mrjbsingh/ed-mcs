77658359104a655b1cdb0c1205d9a8ebd5f61af7
mrjbsingh <jayendra.iiita@gmail.com>
2022-05-01 20:02:05 +0530

    logback.xml added and Logging class info is done

--- microservices/twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/service/config/TwitterToKafkaServiceConfig.java
+++ microservices/twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/service/config/TwitterToKafkaServiceConfig.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Data   //boiler code for getter setter using lombok during compile time
 @Configuration
-@ConfigurationProperties (prefix = "twitter-to-kafka-service:")
+@ConfigurationProperties (prefix = "twitter-to-kafka-service")
 public class TwitterToKafkaServiceConfig {
     private List<String> twitterKeywords;
 }
